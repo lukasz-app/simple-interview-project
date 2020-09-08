779ed8c489d87134fc8a5b8ac16057a16f6c455c
Lukasz <lukasz.chludzinski@gmail.com>
2020-09-09 00:36:07 +0200

    aniamtions

--- src/components/voteButton/VoteButton.tsx
+++ src/components/voteButton/VoteButton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Pressable, Text } from 'react-native';
+import { Pressable, Text, Animated } from 'react-native';
 import { observer } from 'mobx-react';
 import styles from './style';
 
@@ -10,16 +10,52 @@ type Props = {
 };
 
 const VoteButton = ({ type, value, onPress }: Props) => {
+  const [animatedValue] = React.useState(new Animated.Value(0));
+
+  const onMountAnimation = () => {
+    Animated.timing(animatedValue, {
+      toValue: 100,
+      delay: 100,
+      duration: 250,
+      useNativeDriver: true,
+    }).start();
+  };
+  const onClickAnimation = (firstTime = true) => {
+    Animated.timing(animatedValue, {
+      toValue: firstTime ? 150 : 100,
+      duration: 100,
+      useNativeDriver: true,
+    }).start(() => firstTime && onClickAnimation(false));
+  };
+
+  const onButtonPress = () => {
+    onClickAnimation();
+    onPress();
+  };
+
+  React.useEffect(() => {
+    onMountAnimation();
+  }, []);
+
   return (
-    <Pressable
-      style={[
-        styles.container,
-        {
-          backgroundColor: type === 'down' ? 'red' : 'green',
-        },
-      ]}
-      onPress={onPress}>
-      <Text style={styles.text}>{value}</Text>
+    <Pressable onPress={onButtonPress}>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            backgroundColor: type === 'down' ? 'red' : 'green',
+            transform: [
+              {
+                scale: animatedValue.interpolate({
+                  inputRange: [0, 200],
+                  outputRange: [0, 2],
+                }),
+              },
+            ],
+          },
+        ]}>
+        <Text style={styles.text}>{value}</Text>
+      </Animated.View>
     </Pressable>
   );
 };
